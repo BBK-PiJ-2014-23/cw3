560570a9092d1be84bd714d463660b6a84159381
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2014-12-23 23:40:45 +0100

    Refactoring.

--- ArrayListTester.java
+++ ArrayListTester.java
@@ -11,34 +11,33 @@ public class ArrayListTester {
     @Test
     public void test() {
         List arrayList = new ArrayList();
+        
+        // size
         assertTrue(arrayList.isEmpty());
         assertEquals(arrayList.size(), 0);
-
-        ReturnObject index1 = arrayList.get(-1);
-        assertEquals(index1.getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        ReturnObject index2 = arrayList.get(0);
-        assertEquals(index2.getError(), ErrorMessage.EMPTY_STRUCTURE);
-        ReturnObject index3 = arrayList.get(66);
-        assertEquals(index3.getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-
-        ReturnObject nullAdd = arrayList.add(0, null);
-        assertEquals(nullAdd.getError(), ErrorMessage.INVALID_ARGUMENT);
-        ReturnObject addOutOfBounds1 = arrayList.add(-1, "test");
-        assertEquals(addOutOfBounds1.getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        ReturnObject addOutOfBounds2 = arrayList.add(66, "test");
-        assertEquals(addOutOfBounds2.getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        ReturnObject add1 = arrayList.add(0, "test1");
-        assertNull(add1.getReturnValue());
-        ReturnObject add2 = arrayList.add(0, "test2");
-        assertNull(add2.getReturnValue());
+        
+        // get
+        assertEquals(arrayList.get(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(arrayList.get(0).getError(), ErrorMessage.EMPTY_STRUCTURE);
+        assertEquals(arrayList.get(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        
+        // add with index
+        assertEquals(arrayList.add(0, null).getError(), ErrorMessage.INVALID_ARGUMENT);
+        assertEquals(arrayList.add(-1, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(arrayList.add(66, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        
+        assertNull(arrayList.add(0, "test1").getReturnValue());
+        assertNull(arrayList.add(0, "test2").getReturnValue());
         assertEquals(arrayList.get(0).getReturnValue(), "test2");
         assertEquals(arrayList.get(1).getReturnValue(), "test1");
+        
+        // size after
         assertFalse(arrayList.isEmpty());
         assertEquals(arrayList.size(), 2);
         
-        assertEquals(nullAdd.getError(), ErrorMessage.INVALID_ARGUMENT);
-        ReturnObject add3 = arrayList.add("test3");
-        assertNull(add3.getReturnValue());
+        // add plain
+        assertEquals(arrayList.add(null).getError(), ErrorMessage.INVALID_ARGUMENT);
+        assertNull(arrayList.add("test3").getReturnValue());
         assertEquals(arrayList.get(2).getReturnValue(), "test3");
     }
 }
