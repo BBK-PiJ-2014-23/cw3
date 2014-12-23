37cafd1c5f68b5b64259524bd8da5798d30ea2d6
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2014-12-23 16:57:27 +0100

    First commit.

---
+++ ArrayListTester.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * The test class ArrayListTester.
+ *
+ * @author  Stefan E. Mayer
+ * @version 1.0
+ */
+public class ArrayListTester {
+    @Test
+    public void test() {
+        
+    }
+}
