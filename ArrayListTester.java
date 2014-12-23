ba1eed8ae7756d3dda533bd72a407cfc35ea83fd
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2014-12-23 17:48:07 +0100

    Added tests for the get method.

--- ArrayListTester.java
+++ ArrayListTester.java
@@ -13,5 +13,12 @@ public class ArrayListTester {
         List arrayList = new ArrayList();
         assertTrue(arrayList.isEmpty());
         assertEquals(arrayList.size(), 0);
+        
+        ReturnObject index1 = arrayList.get(-1);
+        assertEquals(index1.getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        ReturnObject index2 = arrayList.get(0);
+        assertEquals(index2.getError(), ErrorMessage.EMPTY_STRUCTURE);
+        ReturnObject index3 = arrayList.get(1);
+        assertEquals(index3.getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
     }
 }
