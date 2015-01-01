acb6d14e454f0419d61c20e30fa2d64b0514eea9
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-01 23:35:03 +0100

    Uncommented remaining tests.

--- LinkedListTester.java
+++ LinkedListTester.java
@@ -55,39 +55,36 @@ public class LinkedListTester {
         // prepare for adding with index tests
         assertNull(LinkedList.add("0").getReturnValue());
 
-        //         // add three via index
-        //         assertEquals(LinkedList.add(0, null).getError(), ErrorMessage.INVALID_ARGUMENT);
-        //         assertEquals(LinkedList.add(-1, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(LinkedList.add(66, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertNull(LinkedList.add(0, "1").getReturnValue());
-        //         assertNull(LinkedList.add(0, "2").getReturnValue());
-        //         assertNull(LinkedList.add(2, "3").getReturnValue());
-        //         assertEquals(LinkedList.add(4, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        // 
-        //         // should be 4 elements
-        //         assertFalse(LinkedList.isEmpty());
-        //         assertEquals(LinkedList.size(), 4);
-        // 
-        //         // 4 can be retrieved
-        // 
-        //         assertEquals(LinkedList.get(0).getReturnValue(), "2");
-        //         assertEquals(LinkedList.get(1).getReturnValue(), "1");
-        //         assertEquals(LinkedList.get(2).getReturnValue(), "3");
-        //         assertEquals(LinkedList.get(3).getReturnValue(), "0");
-        // 
-        //         // 4 can be removed
-        // 
-        //         assertEquals(LinkedList.remove(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(LinkedList.remove(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(LinkedList.remove(3).getReturnValue(), "0");
-        //         assertEquals(LinkedList.remove(2).getReturnValue(), "3");
-        //         assertEquals(LinkedList.remove(1).getReturnValue(), "1");
-        //         assertEquals(LinkedList.remove(0).getReturnValue(), "2");
-        //         assertEquals(LinkedList.remove(66).getError(), ErrorMessage.EMPTY_STRUCTURE);
-        // 
-        //         // should be empty now
-        // 
-        //         assertTrue(LinkedList.isEmpty());
-        //         assertEquals(LinkedList.size(), 0);
+        // add three via index
+        assertEquals(LinkedList.add(0, null).getError(), ErrorMessage.INVALID_ARGUMENT);
+        assertEquals(LinkedList.add(-1, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(LinkedList.add(66, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertNull(LinkedList.add(0, "1").getReturnValue());
+        assertNull(LinkedList.add(0, "2").getReturnValue());
+        assertNull(LinkedList.add(2, "3").getReturnValue());
+        assertEquals(LinkedList.add(4, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+
+        // should be 4 elements
+        assertFalse(LinkedList.isEmpty());
+        assertEquals(LinkedList.size(), 4);
+
+        // 4 can be retrieved
+        assertEquals(LinkedList.get(0).getReturnValue(), "2");
+        assertEquals(LinkedList.get(1).getReturnValue(), "1");
+        assertEquals(LinkedList.get(2).getReturnValue(), "3");
+        assertEquals(LinkedList.get(3).getReturnValue(), "0");
+
+        // 4 can be removed
+        assertEquals(LinkedList.remove(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(LinkedList.remove(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(LinkedList.remove(3).getReturnValue(), "0");
+        assertEquals(LinkedList.remove(2).getReturnValue(), "3");
+        assertEquals(LinkedList.remove(1).getReturnValue(), "1");
+        assertEquals(LinkedList.remove(0).getReturnValue(), "2");
+        assertEquals(LinkedList.remove(66).getError(), ErrorMessage.EMPTY_STRUCTURE);
+
+        // should be empty now
+        assertTrue(LinkedList.isEmpty());
+        assertEquals(LinkedList.size(), 0);
     }
 }
