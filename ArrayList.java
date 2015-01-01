e2d523ce1de7c6a90a6e77d91736a33392cab80e
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-01 19:37:30 +0100

    Make method isEmpty use method size.

--- ArrayList.java
+++ ArrayList.java
@@ -35,7 +35,7 @@ public class ArrayList implements List {
      */
     @Override
     public boolean isEmpty() {
-        if (size == 0) {
+        if (size() == 0) {
             return true;
         } else {
             return false;
