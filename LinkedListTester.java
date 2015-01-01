bf3754da9da6c05cb8dfc0801d7ef22e9ab156b6
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-01 22:36:19 +0100

    Format fixes.

--- LinkedListTester.java
+++ LinkedListTester.java
@@ -26,12 +26,12 @@ public class LinkedListTester {
         assertEquals(LinkedList.get(0).getError(), ErrorMessage.EMPTY_STRUCTURE);
         assertEquals(LinkedList.get(66).getError(), ErrorMessage.EMPTY_STRUCTURE);
 
-        //         // add 2 via plain add
+        // add 2 via plain add
         assertEquals(LinkedList.add(null).getError(), ErrorMessage.INVALID_ARGUMENT);
         assertNull(LinkedList.add("0").getReturnValue());
         assertNull(LinkedList.add("1").getReturnValue());
 
-        //         // should be two elements
+        // should be two elements
         assertFalse(LinkedList.isEmpty());
         assertEquals(LinkedList.size(), 2);
 
@@ -40,7 +40,7 @@ public class LinkedListTester {
         //         assertEquals(LinkedList.get(0).getReturnValue(), "0");
         //         assertEquals(LinkedList.get(1).getReturnValue(), "1");
         //         assertEquals(LinkedList.get(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        // 
+
         //         // two can be removed
         //         assertEquals(LinkedList.remove(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
         //         assertEquals(LinkedList.remove(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
