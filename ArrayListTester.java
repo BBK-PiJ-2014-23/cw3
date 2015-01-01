81ff6d5e7695937de6dbaf151ba97a5cb37c9a45
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-01 21:47:02 +0100

    Refactored tests to include add with index texts.

--- ArrayListTester.java
+++ ArrayListTester.java
@@ -5,14 +5,14 @@ import org.junit.Test;
  * The test class ArrayListTester.
  *
  * @author  Stefan E. Mayer
- * @version 1.0
+ * @version 1.1
  */
 public class ArrayListTester {
     @Test
     public void test() {
         List arrayList = new ArrayList();
 
-        // size
+        // size empty
         assertTrue(arrayList.isEmpty());
         assertEquals(arrayList.size(), 0);
 
@@ -26,43 +26,68 @@ public class ArrayListTester {
         assertEquals(arrayList.get(0).getError(), ErrorMessage.EMPTY_STRUCTURE);
         assertEquals(arrayList.get(66).getError(), ErrorMessage.EMPTY_STRUCTURE);
 
-        // add plain
+        // add 2 via plain add
         assertEquals(arrayList.add(null).getError(), ErrorMessage.INVALID_ARGUMENT);
+        assertNull(arrayList.add("0").getReturnValue());
         assertNull(arrayList.add("1").getReturnValue());
 
-        // tests on structure with one element
-
+        // should be two elements
         assertFalse(arrayList.isEmpty());
-        assertEquals(arrayList.size(), 1);
+        assertEquals(arrayList.size(), 2);
 
+        // two can be retrieved
         assertEquals(arrayList.get(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        assertEquals(arrayList.get(0).getReturnValue(), "1");
+        assertEquals(arrayList.get(0).getReturnValue(), "0");
+        assertEquals(arrayList.get(1).getReturnValue(), "1");
         assertEquals(arrayList.get(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
 
+        // two can be removed
         assertEquals(arrayList.remove(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
         assertEquals(arrayList.remove(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(arrayList.remove(0).getReturnValue(), "0");
         assertEquals(arrayList.remove(0).getReturnValue(), "1");
         assertEquals(arrayList.remove(66).getError(), ErrorMessage.EMPTY_STRUCTURE);
 
-    
-        //     // add with index
-        //     assertEquals(arrayList.add(0, null).getError(), ErrorMessage.INVALID_ARGUMENT);
-        //     assertEquals(arrayList.add(-1, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //     assertEquals(arrayList.add(66, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //     
-        //     assertNull(arrayList.add(0, "test1").getReturnValue());
-        //     assertNull(arrayList.add(0, "test2").getReturnValue());
-        //     assertEquals(arrayList.get(0).getReturnValue(), "test2");
-        //     assertEquals(arrayList.get(1).getReturnValue(), "test1");
-        //     
-        //     // size after
-        //     assertFalse(arrayList.isEmpty());
-        //     assertEquals(arrayList.size(), 2);
-        //     
-        //     // remove
-        //     assertEquals(arrayList.remove(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //     assertEquals(arrayList.remove(1).getReturnValue(), "test1");
-        //     assertEquals(arrayList.get(0).getReturnValue(), "test2");
-        //     assertEquals(arrayList.get(1).getReturnValue(), "test3");
+        // should be empty now
+        assertTrue(arrayList.isEmpty());
+        assertEquals(arrayList.size(), 0);
+
+        // prepare for adding with index tests
+        assertNull(arrayList.add("0").getReturnValue());
+
+        // add three via index
+        assertEquals(arrayList.add(0, null).getError(), ErrorMessage.INVALID_ARGUMENT);
+        assertEquals(arrayList.add(-1, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(arrayList.add(66, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertNull(arrayList.add(0, "1").getReturnValue());
+        assertNull(arrayList.add(0, "2").getReturnValue());
+        assertNull(arrayList.add(2, "3").getReturnValue());
+        assertEquals(arrayList.add(4, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+
+        // should be 4 elements
+        assertFalse(arrayList.isEmpty());
+        assertEquals(arrayList.size(), 4);
+
+        // 4 can be retrieved
+
+        assertEquals(arrayList.get(0).getReturnValue(), "2");
+        assertEquals(arrayList.get(1).getReturnValue(), "1");
+        assertEquals(arrayList.get(2).getReturnValue(), "3");
+        assertEquals(arrayList.get(3).getReturnValue(), "0");
+
+        // 4 can be removed
+
+        assertEquals(arrayList.remove(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(arrayList.remove(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(arrayList.remove(3).getReturnValue(), "0");
+        assertEquals(arrayList.remove(2).getReturnValue(), "3");
+        assertEquals(arrayList.remove(1).getReturnValue(), "1");
+        assertEquals(arrayList.remove(0).getReturnValue(), "2");
+        assertEquals(arrayList.remove(66).getError(), ErrorMessage.EMPTY_STRUCTURE);
+
+        // should be empty now
+
+        assertTrue(arrayList.isEmpty());
+        assertEquals(arrayList.size(), 0);
     }
 }
