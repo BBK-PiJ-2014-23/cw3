2f927c1af5de69d31c90cd2f4219bf3963587725
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-01 23:20:06 +0100

    Uncommented some tests for method remove.

--- LinkedListTester.java
+++ LinkedListTester.java
@@ -41,20 +41,20 @@ public class LinkedListTester {
         assertEquals(LinkedList.get(1).getReturnValue(), "1");
         assertEquals(LinkedList.get(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
 
-        //         // two can be removed
-        //         assertEquals(LinkedList.remove(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(LinkedList.remove(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(LinkedList.remove(0).getReturnValue(), "0");
-        //         assertEquals(LinkedList.remove(0).getReturnValue(), "1");
-        //         assertEquals(LinkedList.remove(66).getError(), ErrorMessage.EMPTY_STRUCTURE);
-        // 
-        //         // should be empty now
-        //         assertTrue(LinkedList.isEmpty());
-        //         assertEquals(LinkedList.size(), 0);
-        // 
-        //         // prepare for adding with index tests
-        //         assertNull(LinkedList.add("0").getReturnValue());
-        // 
+        // two can be removed
+        assertEquals(LinkedList.remove(-1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(LinkedList.remove(66).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        assertEquals(LinkedList.remove(0).getReturnValue(), "0");
+        assertEquals(LinkedList.remove(0).getReturnValue(), "1");
+        assertEquals(LinkedList.remove(66).getError(), ErrorMessage.EMPTY_STRUCTURE);
+
+        // should be empty now
+        assertTrue(LinkedList.isEmpty());
+        assertEquals(LinkedList.size(), 0);
+
+        // prepare for adding with index tests
+        assertNull(LinkedList.add("0").getReturnValue());
+
         //         // add three via index
         //         assertEquals(LinkedList.add(0, null).getError(), ErrorMessage.INVALID_ARGUMENT);
         //         assertEquals(LinkedList.add(-1, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
