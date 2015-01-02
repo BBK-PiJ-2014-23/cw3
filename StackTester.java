bccce7adf24c8fdad1a366b7c2bd5d8ade68d411
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-02 19:42:43 +0100

    Added remaining tests.

--- StackTester.java
+++ StackTester.java
@@ -30,60 +30,30 @@ public class StackTester {
         // should be one element
         assertFalse(stack.isEmpty());
         assertEquals(stack.size(), 1);
-        
+
         // push another element
         stack.push("1");
         assertEquals(stack.top().getReturnValue(), "1");
-        
+
         // should be two elements now
         assertFalse(stack.isEmpty());
         assertEquals(stack.size(), 2);
-        
-        // 
-        //         // two can be removed
-        //         assertEquals(stack.pop().getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(stack.pop().getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(stack.pop().getReturnValue(), "0");
-        //         assertEquals(stack.pop().getReturnValue(), "1");
-        //         assertEquals(stack.pop().getError(), ErrorMessage.EMPTY_STRUCTURE);
-        // 
-        //         // should be empty now
-        //         assertTrue(stack.isEmpty());
-        //         assertEquals(stack.size(), 0);
-        // 
-        //         // prepare for adding with index tests
-        //         //assertNull(stack.push("0").getReturnValue());
-        // 
-        //         // add three via index
-        //         //assertEquals(stack.push(0, null).getError(), ErrorMessage.INVALID_ARGUMENT);
-        //         //assertEquals(stack.push(-1, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         //assertEquals(stack.push(66, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         //assertNull(stack.push(0, "1").getReturnValue());
-        //         //assertNull(stack.push(0, "2").getReturnValue());
-        //         //assertNull(stack.push(2, "3").getReturnValue());
-        //         //assertEquals(stack.push(4, "test").getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        // 
-        //         // should be 4 elements
-        //         assertFalse(stack.isEmpty());
-        //         assertEquals(stack.size(), 4);
-        // 
-        //         // 4 can be retrieved
-        //         assertEquals(stack.top().getReturnValue(), "2");
-        //         assertEquals(stack.top().getReturnValue(), "1");
-        //         assertEquals(stack.top().getReturnValue(), "3");
-        //         assertEquals(stack.top().getReturnValue(), "0");
-        // 
-        //         // 4 can be removed
-        //         assertEquals(stack.pop().getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(stack.pop().getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
-        //         assertEquals(stack.pop().getReturnValue(), "0");
-        //         assertEquals(stack.pop().getReturnValue(), "3");
-        //         assertEquals(stack.pop().getReturnValue(), "1");
-        //         assertEquals(stack.pop().getReturnValue(), "2");
-        //         assertEquals(stack.pop().getError(), ErrorMessage.EMPTY_STRUCTURE);
-        // 
-        //         // should be empty now
-        //         assertTrue(stack.isEmpty());
-        //         assertEquals(stack.size(), 0);
+
+        // removing one
+        assertEquals(stack.pop().getReturnValue(), "1");
+
+        // should be one element now
+        assertFalse(stack.isEmpty());
+        assertEquals(stack.size(), 1);
+        assertEquals(stack.top().getReturnValue(), "0");
+
+        // pop another element
+        assertEquals(stack.pop().getReturnValue(), "0");
+
+        //stack is empty now
+        assertTrue(stack.isEmpty());
+        assertEquals(stack.size(), 0);
+        assertEquals(stack.pop().getError(), ErrorMessage.EMPTY_STRUCTURE);
+        assertEquals(stack.top().getError(), ErrorMessage.EMPTY_STRUCTURE);
     }
 }
