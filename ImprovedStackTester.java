e5c2e8f2a71fe06eb3a56058ee0afce45331ce47
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-28 12:07:45 +0000

    Added test to test the reversed stack method.

--- ImprovedStackTester.java
+++ ImprovedStackTester.java
@@ -12,13 +12,16 @@ public class ImprovedStackTester {
     public void test() {
         List list = new ArrayList();
         ImprovedStack improvedStack = new ImprovedStackImpl(list);
-        
+
         // size empty
+        assertTrue(improvedStack.isEmpty());
+        assertEquals(improvedStack.size(), 0);
         assertTrue(improvedStack.isEmpty());
         assertEquals(improvedStack.size(), 0);
 
         // top on empty structure
         assertEquals(improvedStack.top().getError(), ErrorMessage.EMPTY_STRUCTURE);
+        assertEquals(improvedStack.reverse().top().getError(), ErrorMessage.EMPTY_STRUCTURE);
 
         // pop on empty structure
         assertEquals(improvedStack.pop().getError(), ErrorMessage.EMPTY_STRUCTURE);
@@ -26,18 +29,31 @@ public class ImprovedStackTester {
         // push 1 and top
         improvedStack.push("0");
         assertEquals(improvedStack.top().getReturnValue(), "0");
+        
+        // top on reverse stack
+        assertEquals(improvedStack.reverse().top().getReturnValue(), "0");
+        
+        // should be one element in reverse stack
+        assertFalse(improvedStack.isEmpty());
+        assertEquals(improvedStack.size(), 1);
 
         // should be one element
         assertFalse(improvedStack.isEmpty());
         assertEquals(improvedStack.size(), 1);
-
+        
         // push another element
         improvedStack.push("1");
         assertEquals(improvedStack.top().getReturnValue(), "1");
+        
+        // should be two elements in reverse stack now
+        assertFalse(improvedStack.isEmpty());
+        assertEquals(improvedStack.size(), 2);
+        assertEquals(improvedStack.reverse().top().getReturnValue(), "0");
 
         // should be two elements now
         assertFalse(improvedStack.isEmpty());
         assertEquals(improvedStack.size(), 2);
+        assertEquals(improvedStack.top().getReturnValue(), "1");
 
         // removing one
         assertEquals(improvedStack.pop().getReturnValue(), "1");
@@ -46,7 +62,10 @@ public class ImprovedStackTester {
         assertFalse(improvedStack.isEmpty());
         assertEquals(improvedStack.size(), 1);
         assertEquals(improvedStack.top().getReturnValue(), "0");
-
+        assertFalse(improvedStack.reverse().isEmpty());
+        assertEquals(improvedStack.reverse().size(), 1);
+        assertEquals(improvedStack.reverse().top().getReturnValue(), "0");
+        
         // pop another element
         assertEquals(improvedStack.pop().getReturnValue(), "0");
 
@@ -55,5 +74,9 @@ public class ImprovedStackTester {
         assertEquals(improvedStack.size(), 0);
         assertEquals(improvedStack.pop().getError(), ErrorMessage.EMPTY_STRUCTURE);
         assertEquals(improvedStack.top().getError(), ErrorMessage.EMPTY_STRUCTURE);
+        assertTrue(improvedStack.reverse().isEmpty());
+        assertEquals(improvedStack.reverse().size(), 0);
+        assertEquals(improvedStack.reverse().pop().getError(), ErrorMessage.EMPTY_STRUCTURE);
+        assertEquals(improvedStack.reverse().top().getError(), ErrorMessage.EMPTY_STRUCTURE);
     }
 }
