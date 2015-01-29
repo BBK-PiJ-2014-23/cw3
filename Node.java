3c45365ff1a6b49a6a091ca3d7cf827cae180e6f
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-29 11:13:53 +0000

    Nodes in linked lists do not need indexes. Adjusted class Node accordingly.

--- Node.java
+++ Node.java
@@ -1,17 +1,14 @@
 public class Node {
     private Object object;
-    private int index;
     private Node next;
     
     /**
      * Constructor creating a new node object.
      * 
      * @param object the object to be included in the node
-     * @param index the postition of the node within a list
      */
-    public Node(Object object, int index) {
+    public Node(Object object) {
         this.object = object;
-        this.index = index;
         this.next = null;
     }
     
@@ -22,24 +19,6 @@ public class Node {
      */
     public Object getObject() {
         return object;
-    }
-    
-    /**
-     * Setter for the position of the node within a list.
-     * 
-     * @param index the position of the node within a list
-     */
-    public void setIndex(int index) {
-        this.index = index;
-    }
-    
-    /**
-     * Getter for the position of the node within a list.
-     * 
-     * @return the position of the node within a list
-     */
-    public int getIndex() {
-        return index;
     }
     
     /**
