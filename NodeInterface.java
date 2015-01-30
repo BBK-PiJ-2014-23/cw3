4260ecb08484035e4750af33bac32e3f889477d9
BBK-PiJ-2014-23 <smayer@mail.bbk.ac.uk>
2015-01-30 17:08:30 +0000

    First commit.

---
+++ NodeInterface.java
@@ -0,0 +1,26 @@
+/**
+ * This node is a container that stores an object of any kind and points to another node.
+ * It can be used in Linked Lists.
+ */
+public interface NodeInterface {
+    /**
+     * Getter for the object included in the node.
+     * 
+     * @return the object within the node
+     */
+    public Object getObject();
+    
+    /**
+     * Setter for the next node in a list.
+     * 
+     * @param next the next node in a list
+     */
+    public void setNext(Node next);
+    
+    /**
+     * Getter for the next node in in a list.
+     * 
+     * @return the next node in a list
+     */
+    public Node getNext();
+}
